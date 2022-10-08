351f61d745684acbe597f5e6985759d03fe4b401
Riju Basu <whitedominator17@gmail.com>
2022-10-08 18:43:55 +0530

    new

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -3,3 +3,4 @@ export { default as Feed } from './Feed';
 export { default as ChannelDetail } from "./ChannelDetail";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as VideodDetail } from "./VideodDetail";
+export { default as SearchBar } from "./SearchBar";
